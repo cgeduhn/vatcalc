1dff112a0125b5467c495fb5a22ceffbfd0eb00c
Christopher Geduhn <cgeduhn@Christophers-iMac.local>
2017-10-12 17:03:57 +0200

    tests for bill

--- lib/vatcalc/bill.rb
+++ lib/vatcalc/bill.rb
@@ -1,35 +1,179 @@
-module Vatcalc
-  class Bill < Numeric
 
-    include Comparable
+module Vatcalc    
+  class Bill 
 
-    delegate :rates,:rates_changed?,:currency,:vat_percentages, to: :base
+    
 
-    attr_accessor :base,:total,:services
+    attr_reader :base_elements
+    attr_reader :service_elements
+    attr_reader :elements
+    attr_reader :total
+    attr_reader :currency
 
-    [:base=,:services=].each {|i| private i}
+    delegate :gross,:net,:vat, to: :total
 
-
-    def initialize()
-      @base = Base.new
-      @services = Hash.new(0)
+    def initialize(options={})
+      @vat_percentages = Set.new
+      @base_elements    = insert_by_option_argument( options.to_h.fetch(:base),        :obj_to_base_element)
+      @service_elements = insert_by_option_argument( options.to_h.fetch(:services,[]), :obj_to_service_element)
     end
 
-    def <<(obj)
-      insert(obj)
+    def elements 
+      @base_elements + @service_elements
     end
 
-    def insert(obj,quantity=1)
-      case oth
-      when ServiceElement
-        @services.insert(obj,@services.size,quantity)
-      when BaseElement
-        @base.insert(obj,quantity)
-      else 
-        # TODO definie if obj respond_to_ acts_as_vat_service or acts_as_base_element ? 
+    def vat_percentages
+      @vat_percentages.to_a
+    end
+
+    def vat_splitted
+      @vat_splitted ||= money_hash.tap do |result_hash|
+        @base_elements.each {|elem,q| result_hash[elem.vat_p] += (elem*q).vat}
+        @service_elements.each {|elem,q| elem.vat_splitted.each {|vp,vat| result_hash[vp] += vat} }
       end
-      return self
     end
+
+    # Output of rates in form of
+    # key is VAT Percentage and Value is the rate 
+    # "{1.0=>0.0092, 1.19=>0.8804, 1.07=>0.1104}"
+    # "{1.0=>0.4927, 1.19=>0.0168, 1.07=>0.4905}"
+    # "{1.0=>0.1284, 1.19=>0.6933, 1.07=>0.1783}"
+    # "{1.0=>0.8215, 1.19=>0.1618, 1.07=>0.0167}"
+    # "{1.0=>0.531,  1.19=>0.0866, 1.07=>0.3824}"
+    # "{1.0=>0.1152, 1.19=>0.0113, 1.07=>0.8735}"
+    # "{1.0=>0.1927, 1.19=>0.4567, 1.07=>0.3506}"
+    # "{1.0=>0.797,  1.19=>0.1609, 1.07=>0.0421}"
+    # "{1.0=>0.2525, 1.19=>0.0036, 1.07=>0.7439}"
+    # "{1.0=>0.2475, 1.19=>0.3371, 1.07=>0.4154}"
+    # "{1.0=>0.1739, 1.19=>0.5261, 1.07=>0.3}"
+    def rates
+      @rates ||= rates!
+    end
+
+
+    RoundPrecision = 4
+    #will only be used in rspec for test
+    Tolerance = BigDecimal("1E-#{RoundPrecision}")
+    #@see +rates+
+    def rates!
+      @rates = Hash.new(0.00)
+      if net != 0 
+        left_over = 1.00
+        grouped_amounts = @base_elements.inject(money_hash){ |h,(elem,q)| h[elem.vat_p] += elem.net * q; h}.sort
+
+        grouped_amounts.each_with_index do |(vp,amount),i|
+          if i == (grouped_amounts.length - 1)
+            #last element
+            @rates[vp] = left_over.round(RoundPrecision)
+          else
+            @rates[vp] = (amount / net).round(RoundPrecision)
+            left_over -= @rates[vp]
+          end
+        end
+      else
+        max_p = vat_percentages.max
+        @rates = @grouped_amounts.each { |(vp,gnv)| @rates[vp] = 0.00 }
+        @rates[max_p] = 1.00 if max_p
+      end
+      @rates
+    end
+
+
+    # Output of rates in form of
+    # key is VAT Percentage and Value is the rate in decimal form
+    # {0.0=>75.77, 19.0=>20.91, 7.0=>3.32}
+    # {0.0=>60.29, 19.0=>2.61,  7.0=>37.1}"
+    # {0.0=>12.17, 19.0=>83.0,  7.0=>4.83}"
+    # {0.0=>75.82, 19.0=>5.07,  7.0=>19.11}"
+    # {0.0=>32.42, 19.0=>22.93, 7.0=>44.65}"
+    # {0.0=>0.26,  19.0=>32.83, 7.0=>66.91}"
+    # {0.0=>40.38, 19.0=>51.67, 7.0=>7.95}"
+    # {0.0=>11.56, 19.0=>10.71, 7.0=>77.73}"
+    def human_rates
+      #example ((1.19 - 1.00)*100).round(2) => 19.0
+      rates.inject({}){|h,(pr,v)| h[((pr.to_f-1.00)*100).round(RoundPrecision-2)] = (v*100).round(RoundPrecision-2); h}
+    end
+
+
+    alias_method :percentages, :vat_percentages
+    alias_method :vat_rates, :rates
+
+
+    private 
+
+    def insert_by_option_argument(arg,convert_method)
+      case arg
+      when Array
+        arg.collect{|i| insert(i,convert_method) }
+      else
+        [insert(arg,convert_method)]
+      end
+    end
+
+
+    def insert(obj, convert_method) 
+      case obj
+      when Hash
+        quantity = obj[:quantity]
+      when Array
+        quantity = obj[1]
+        obj = obj[0]
+      end
+      
+      obj = send(convert_method,obj) 
+      quantity ||= 1
+
+      if quantity.to_i > 0
+        gnv = quantity.to_i == 1 ? obj.to_gnv : obj.to_gnv * quantity
+
+        @total ? @total += gnv : @total = gnv
+
+        case obj
+        when BaseElement
+          @vat_percentages << obj.vat_percentage
+        when ServiceElement
+        end
+        @currency = obj.currency
+
+        #add or set gnv to the vat_percentage key
+      else 
+        raise ArgumentError.new "quantity must be != 0"
+      end
+
+      [obj,quantity]
+    end
+
+    def obj_to_base_element(obj)
+      case obj
+      when BaseElement
+        obj
+      when Numeric,Money
+        BaseElement.new(obj)
+      when Hash
+        BaseElement.new(obj[:amount] || obj[:gross] || obj[:value], obj)
+      else
+        raise TypeError.new "#{obj} can't be converted into a BaseElement"
+      end
+    end
+
+    def obj_to_service_element(obj)
+      case obj
+      when ServiceElement
+        obj.rates = rates
+        obj
+      when Numeric,Money
+        ServiceElement.new(obj,rates)
+      when Hash
+        ServiceElement.new(obj[:amount] || obj[:gross] || obj[:value],rates)
+      else
+        raise TypeError.new "#{obj} can't be converted into a ServiceElement"
+      end
+    end
+
+    def money_hash
+      Hash.new(Money.new(0,@currency))
+    end
+
 
 
   end
---
+++ spec/bill_spec.rb
@@ -0,0 +1,187 @@
+require "spec_helper"
+
+RSpec.describe Vatcalc::Bill do
+
+  let(:tolerance) { Vatcalc::Bill::Tolerance }
+  let(:round_precision) { Vatcalc::Base::RoundPrecision }
+
+
+
+  describe "containing only base elements" do
+
+    let (:b) {
+      Vatcalc::Bill.new(base: [
+        {amount: 100.00,vat_percentage: 7},
+        100,
+        {percentage: 19.00,value: 100.00}
+      ])
+    }
+
+    it "inserted items correctly" do
+      expect(b.gross.to_f).to eq(201.00)
+      expect(b.net.to_f).to eq(178.33)
+      expect(b.vat.to_f).to eq((201.00 - 178.33).round(2))
+      expect(b.percentages.length).to eq(2)
+    end
+
+    it "has correct items" do 
+      expect(b.elements.length).to eq(3)
+      expect(b.base_elements.length).to eq(3)
+      expect(b.service_elements.length).to eq(0)
+    end
+
+    it "has correct rates" do
+      expect(b.rates.length).to eq(2)
+      expect(b.rates.values.sort).to eq([0.4759,0.5241])
+      d = (b.rates.values.sum.round(round_precision))
+      expect(1.00 - d).to be <= tolerance
+    end
+  end
+
+  describe "containing 2 elements with no vat percentage" do
+    let (:b) {
+      Vatcalc::Bill.new(base: [
+        {amount: 50.45,vat_percentage: 0},
+        {percentage: 0,value: 45.45}
+      ])
+    }
+    it "calculates correctly the sum" do 
+      expect(b.gross.to_f).to eq(95.90)
+      expect(b.net.to_f).to eq(95.90)
+      expect(b.vat.to_f).to eq(0)
+      expect(b.rates.values).to eq([1.00])
+      expect(b.rates.keys.collect(&:to_f)).to eq([1.00])
+    end
+  end
+
+  describe "containing 2 elements with vat percentage of 7" do
+    let(:obj1) { Vatcalc::BaseElement.new(47.47,percentage: 7) }
+    let(:obj2) { Vatcalc::BaseElement.new(45.45,percentage: 7) }
+
+    let(:b) { Vatcalc::Bill.new(base: [[obj1,1],[obj2,1]]) }
+
+    it "calculates correctly the sum" do 
+      expect(b.gross.to_f).to eq(92.92)
+      expect(b.net.to_f).to eq(86.84)
+      expect(b.vat.to_f).to eq(6.08)
+    end
+  end
+
+  describe "calculating random rates" do
+    it "has correctly rates" do
+      r = Proc.new{|it| rand(100000).to_f * (rand*100)}
+      100.times do |i|
+        b = Vatcalc::Bill.new(base: [
+          {value: r.call,vat_percentage: 0.00, quantity: 2},
+          {value: r.call,vat_percentage: 7, quantity: 2},
+          {value: r.call,vat_percentage: 19,   quantity: 2}
+        ])
+        d = (b.rates.values.sum.to_d(Vatcalc::Base::RoundPrecision))
+        expect(1.00 - d).to be <= (Vatcalc::Base::Tolerance)
+      end
+    end
+  end
+
+
+  describe "containing base elements and service_elements" do
+
+    let(:elem1) {Vatcalc::BaseElement.new(10.00, percentage: 19, net: true)}
+    let(:elem2) {Vatcalc::BaseElement.new(10.00, percentage:  7, net: true)}
+
+    let (:s) { Vatcalc::ServiceElement.new(5.00) }
+    let(:b) { Vatcalc::Bill.new(base: [elem1,elem2], services: [s]) }
+
+    let(:s_net) { Money.euro((2.5/1.19)*100) + Money.euro(100*(2.5/1.07)) }
+
+
+    it "has correctly gross" do
+      expect(b.gross.to_f).to eq(27.60)
+    end
+
+    it "has correctly net" do
+      expect(b.net).to eq(s_net + (2 * Money.euro(10 * 100)))
+    end
+
+    it "has correctly vat" do
+      expect(b.vat.to_f).to eq( (Money.euro(56) + Money.euro(1.9 * 100) + Money.euro(0.70 * 100)).to_f )
+    end
+
+    it "has correctly vat splitting" do
+
+      expect(b.vat_splitted).to eq({
+        Vatcalc::VATPercentage.new(19) => elem1.vat + Money.euro(40),
+        Vatcalc::VATPercentage.new(7) =>  elem2.vat + Money.euro(16),
+      })
+
+
+      expect(s.vat_splitted).to eq({
+        Vatcalc::VATPercentage.new(19) => Money.euro(40),
+        Vatcalc::VATPercentage.new(7) =>  Money.euro(16)
+      })
+    end
+
+
+  end
+
+end
+
+
+
+  # let (:s) {Vatcalc::ServiceElement}
+  # let (:b) {Vatcalc::Base.new}
+
+
+  # describe "with a simple base with VAT percentage of 19" do 
+  #   let(:elem) {Vatcalc::BaseElement.new(10.00,percentage: 19)}
+  #   let (:b) {Vatcalc::Base.new.insert(elem)}
+
+
+  #   let (:s) {Vatcalc::ServiceElement.new(5.00,b.rates)}
+
+  #   it "has correctly net" do
+  #     expect(s.net.to_f).to eq(4.2)
+  #   end
+
+  #   it "has correctly vat" do
+  #     expect(s.vat.to_f).to eq(0.8)
+  #   end
+
+  #   it "has a correctly vat splitting" do
+  #     expect(s.vat_splitted).to eq({Vatcalc::VATPercentage.new(19) => Money.euro(80)})
+  #   end
+
+  # end
+
+
+  # describe "with a base with VAT percentage of 19 and 7" do 
+  #   let(:elem1) {Vatcalc::BaseElement.new(9.99, percentage: 19)}
+  #   let(:elem2) {Vatcalc::BaseElement.new(9.99, percentage:  7)}
+  #   let(:elem3) {Vatcalc::BaseElement.new(9.99, percentage:  0)}
+
+  #   let (:b) {a = Vatcalc::Base.new.insert(elem1); a.insert(elem2); a.insert(elem3);}
+
+  #   #9.99 / 1.19 = 8.39 # => 0.302669 5526695527 # =>   0.302667
+  #   #9.99 / 1.07 = 9.34 # => 0.336940 83694083693 # =>  0.336941
+  #   #9.99 / 1.00 = 9.99 # => 0.360389 6103896104 # =>   0.360390
+
+  #   # => 27.72 net
+  #   # => 02.25 vat
+  #   # => 29.97 gross 
+
+
+  #   #Coupon 10%
+  #   let (:s) {Vatcalc::ServiceElement.new(-3.00,b.rates)}
+
+  #   let (:m) { Money.euro(-3*100).allocate([0.30267,0.336941,0.360389]) }
+
+  #   let (:expected_net) {  m[0]/Vatcalc::VATPercentage.new(19) + m[1]/Vatcalc::VATPercentage.new(7) + m[2] }
+
+  #   it "has correctly net" do
+  #     expect(s.net).to eq(expected_net)
+  #end
+
+
+
+
+
+
